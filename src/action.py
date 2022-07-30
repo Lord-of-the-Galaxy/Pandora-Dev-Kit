f07f14cd122225cb6c85b31f81ea07606bf747ff
Lord-of-the-Galaxy <25320402+Lord-of-the-Galaxy@users.noreply.github.com>
2022-07-30 22:20:09 +0530

    Remove unnecessary code

--- src/action.py
+++ src/action.py
@@ -67,32 +67,3 @@ class Action:
             self.entity.build(self._build_type)
         elif self._build and isinstance(self.entity, Base):
             self.entity.build(self._build_type)
-
-    def encode(self) -> str:
-        s = self._move.value
-        if self._mine is not None:
-            s += 'M' + self._mine.value
-        elif self._cargo is not None:
-            s += 'C' + ''.join([res.value for res in self._cargo])
-        elif self._build:
-            s += 'B' + ('' if self._build_type is None else self._build_type.desc)
-        return s
-
-    def decode(self, s: str) -> Action:
-        try:
-            self._move = Direction(s[0])
-            if s[1] == 'M' and isinstance(self.entity, Miner):
-                self._mine = Direction(s[2])
-            elif s[1] == 'C' and isinstance(self.entity, Miner):
-                self._cargo = [Resource(c) for c in s[2:]]
-            elif s[1] == 'B':
-                build_type = Entity.find_type(s[2])
-                if isinstance(self.entity, Base) and issubclass(build_type, Ship):
-                    self._build = True
-                    self._build_type = build_type
-                elif isinstance(self.entity, Miner) and issubclass(build_type, Base | Turret):
-                    self._build = True
-                    self._build_type = build_type
-        finally:
-            # if we have an error, we don't care - even valid actions may cause an exception but still decode correctly
-            return self
